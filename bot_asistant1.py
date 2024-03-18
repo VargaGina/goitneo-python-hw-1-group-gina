a8ab624fea6af2fabf10460ef13b6682d873b16a
VargaGina <popgina29@yahoo.com>
2024-03-18 17:03:07 +0200

    bot_assistant1.py

---
+++ bot_asistant1.py
@@ -0,0 +1,58 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+def change_contact(args, contacts):
+    name, new_phone=args
+    if name in contacts:
+        contacts[name]=new_phone
+        return f"Phone number for {name} is updated to {new_phone}"
+    else:
+        return f"Contact '{name}' not found."
+
+def display_contact(args, contacts):
+    name=args[0]
+    if name in contacts:
+        return f"Phone number for {name} is {contacts[name]}"
+    else:
+        return f"Contact not found"
+    
+def display_all(contacts):
+    output=""
+    for name, phone in contacts.items():
+        output+= f"{name}:{phone}\n"
+    return output
+    
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command=="change":
+            print (change_contact(args, contacts))
+        elif command=="phone":
+            print(display_contact(args, contacts))
+        elif command=="all":
+            print(display_all(contacts))
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
+
+
